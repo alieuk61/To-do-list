965d896d0f7e446157b5966705bb6035c31a22e8
codewarswarrior <alieuk61@gmail.com>
2023-10-09 03:52:11 +0100

    another commit

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './todo/todo.css';
+import './todo/mobile-todo.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
