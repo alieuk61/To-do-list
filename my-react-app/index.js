7322b97929ca9a97bde6a70ef908d16ea782c981
codewarswarrior <alieuk61@gmail.com>
2023-10-11 21:36:06 +0100

    another commit

--- my-react-app/index.js
+++ my-react-app/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './todo/todo.css';
-import './todo/mobile-todo.css'
+import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './src/reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
